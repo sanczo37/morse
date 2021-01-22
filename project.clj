aff829e7841e56be082759c45f6c938050a13d73
Sebastian <73378416+sanczo37@users.noreply.github.com>
2021-01-22 18:07:24 +0100

    Add files via upload

---
+++ project.clj
@@ -0,0 +1,12 @@
+(defproject zalka "0.1.0-SNAPSHOT"
+  :description "Decode Morse code missing chars/words separators"
+  :signing {:gpg-key "Hashobject Ltd <team@hashobject.com>"}
+  :url "https://github.com/hashobject/translate"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.7.0"]]
+  :main zalka.core)
+
+
+
+
---
+++ test/zalka/core_test.clj
@@ -0,0 +1,7 @@
+(ns zalka.core-test
+  (:require [clojure.test :refer :all]
+            [zalka.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
